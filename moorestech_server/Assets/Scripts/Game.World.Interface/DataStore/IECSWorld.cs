d79f782c7c669ba1efe7f77270348d5b1382a1ef
Juha <kukiyomenaisapp@gmail.com>
2024-03-02 15:50:52 +0900

    ECSWorldを作成

---
+++ moorestech_server/Assets/Scripts/Game.World.Interface/DataStore/IECSWorld.cs
@@ -0,0 +1,7 @@
+﻿namespace Game.World.Interface.DataStore
+{
+    public interface IECSWorld
+    {
+        public Arch.Core.World World { get; }
+    }
+}
---
+++ moorestech_server/Assets/Scripts/Game.World/DataStore/ECSWorld.cs
@@ -0,0 +1,14 @@
+﻿using Game.World.Interface.DataStore;
+
+namespace Game.World.DataStore
+{
+    public class ECSWorld : IECSWorld
+    {
+        public Arch.Core.World World { get; }
+
+        public ECSWorld(Arch.Core.World world)
+        {
+            World = world;
+        }
+    }
+}
