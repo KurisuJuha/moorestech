a8ccffefc599730c19f2135dfacc9253ee9f2586
Juha <kukiyomenaisapp@gmail.com>
2024-03-04 15:07:49 +0900

    DestroyLogicを作成

---
+++ moorestech_server/Assets/Scripts/Game.Systems/DestroyLogic.cs
@@ -0,0 +1,17 @@
+﻿using Arch.Core;
+using Game.SystemScheduler;
+
+namespace Game.Systems
+{
+    public struct Destroy
+    {
+    }
+
+    public static class DestroyLogic
+    {
+        public static void Update(ref SystemState state)
+        {
+            state.World.Destroy(new QueryDescription().WithAll<Destroy>());
+        }
+    }
+}
